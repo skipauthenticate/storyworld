51d740e5e0bbfc8abb9f780025d6ee06524dc462
Vishnu Karpuram <skipauthenticate@gmail.com>
2026-09-22 13:10:07 -0700

    Delete supabase/migrations/20260309031217_e7a4a2da-5581-4036-9d82-0a8ad15f17e0.sql

--- supabase/migrations/20260309031217_e7a4a2da-5581-4036-9d82-0a8ad15f17e0.sql
+++
@@ -1,6 +0,0 @@
-DROP POLICY "Service role insert experiments" ON public.experiments;
-DROP POLICY "Service role update experiments" ON public.experiments;
-CREATE POLICY "Service role insert experiments" ON public.experiments
-  FOR INSERT TO service_role WITH CHECK (true);
-CREATE POLICY "Service role update experiments" ON public.experiments
-  FOR UPDATE TO service_role USING (true);
